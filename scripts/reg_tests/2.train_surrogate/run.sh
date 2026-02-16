4e99591d549382e024601a3da0bcc0a41d4d8cca
Malik <malik.hassanaly@gmail.com>
2026-02-16 13:47:10 -0700

    add comments in the run scripts

--- scripts/reg_tests/2.train_surrogate/run.sh
+++ scripts/reg_tests/2.train_surrogate/run.sh
@@ -1,3 +1,6 @@
+# Save test and train dataset separately and consistently
 python prepare_datasets.py training_recipes/recipe.yml
+# Train surrogate
 python train_nn.py training_recipes/recipe.yml
+# Test surrogate
 python test_nn.py training_recipes/recipe.yml
--- scripts/reg_tests/3.surrogate_mcmc/run.sh
+++ scripts/reg_tests/3.surrogate_mcmc/run.sh
@@ -1,4 +1,6 @@
 root_dir=`python -c "from batfit import BATFIT_DIR; print(BATFIT_DIR)"`
 n_procs=4
+# Parallel MCMC (1 worker per optimization)
 mpiexec -n $n_procs python bayesCal_synth_parallel.py cal_recipes/recipe.yml
+# Test MCMC results
 python test_mcmc.py cal_recipes/recipe.yml
