5d295cfacc4ccbc3d64709eb611cf89a3ce688fc
Malik <malik.hassanaly@gmail.com>
2026-02-16 14:42:09 -0700

    add Readme

---
+++ scripts/reg_tests/3.surrogate_mcmc/run_change_recipe_local.sh
@@ -0,0 +1,5 @@
+in_yml=cal_recipes/recipe_rel.yml
+out_yml=cal_recipes/recipe.yml
+r_path=/Users/mhassana/Desktop/GitHub/BatFit/scripts/reg_tests
+
+python write_absolute_recipe.py $in_yml $out_yml $r_path
