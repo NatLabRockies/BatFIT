8afe223e1d2f0f782ce06a7924f6dc03908ec182
Malik <malik.hassanaly@gmail.com>
2026-05-11 09:27:06 -0600

    remove metrics from paramNN

--- batfit/model/paramNN.py
+++ batfit/model/paramNN.py
@@ -32,39 +32,8 @@ from batfit.utils.torch_utils import (
 )
 
 from .ae import AECNN
+from .metrics import accuracy, identifiability, rel_accuracy
 from .vae import VAECNN
-
-
-def identifiability(output_std):
-    """
-    Compute identifiability 1/std of prediction
-    output_std: Predicted parameter marginal standard deviation
-    """
-    return torch.mean(1 / output_std, axis=0)
-
-
-def accuracy(output_mean, target):
-    """
-    mean absolute error
-    output_mean: Predicted parameter mean
-    target: Ground truth labels.
-    """
-    acc = torch.mean(abs(output_mean - target), axis=0)
-    return acc
-
-
-def rel_accuracy(output_mean, target):
-    """
-    mean relative absolute error
-    output_mean: Predicted parameter mean
-    target: Ground truth labels.
-    """
-    racc = torch.mean(
-        abs(output_mean - target)
-        / torch.maximum(abs(output_mean), abs(target)),
-        axis=0,
-    )
-    return racc
 
 
 def mse_loss(output, target):
