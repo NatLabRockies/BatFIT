b2bbe8ae6cd2a04627ce20c4376d879144d20788
Malik <malik.hassanaly@gmail.com>
2026-09-21 14:31:39 -0600

    remove old validation pipeline

--- scripts/reg_tests/1.gen_data/run_val_p2d.sh
+++
@@ -1,7 +0,0 @@
-root_dir=`python -c "from batfit import BATFIT_DIR; print(BATFIT_DIR)"`
-n_procs=4
-yaml_in=${root_dir}/default_exps/p2d_discharge_6par.yaml
-folder_save_val=./data_p2d_discharge_val
-# Generate data
-mpiexec -n $n_procs python gen_sol.py -sim_config $yaml_in -folder_save $folder_save_val 
-
