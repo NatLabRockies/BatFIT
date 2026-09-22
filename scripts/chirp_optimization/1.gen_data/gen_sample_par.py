cf1e51282739b6c2bab626e5b7cdaf086094b388
Malik <malik.hassanaly@gmail.com>
2026-09-22 09:35:06 -0600

    gen data for chirp optimization

---
+++ scripts/chirp_optimization/1.gen_data/gen_sample_par.py
@@ -0,0 +1,98 @@
+import argparse
+import os
+import sys
+
+from batfit import BATFIT_EXP, logger
+from batfit.preprocess.param_sampling import *
+from batfit.preprocess.sim_setup import make_params
+
+parser = argparse.ArgumentParser(description="Parameter sampling (chirp mode)")
+parser.add_argument(
+    "-n_int",
+    "--n_int",
+    type=int,
+    metavar="",
+    required=False,
+    help="Number of interior samples",
+    default=10,
+)
+parser.add_argument(
+    "-n_bound",
+    "--n_bound",
+    type=int,
+    metavar="",
+    required=False,
+    help="Number of boundary samples",
+    default=0,
+)
+parser.add_argument(
+    "-sim_config",
+    "--sim_config",
+    type=str,
+    metavar="",
+    required=False,
+    help="Sim config file",
+    default=os.path.join(BATFIT_EXP, "spm_chirp.yaml"),
+)
+parser.add_argument(
+    "-folder_save",
+    "--folder_save",
+    type=str,
+    metavar="",
+    required=False,
+    help="Data folder",
+    default=".",
+)
+args, unknown = parser.parse_known_args()
+if len(unknown) > 0:
+    logger.warning(f"Unknown args {unknown}")
+
+n_int = args.n_int
+n_bound = args.n_bound
+li_cons = True
+sim_params = make_params(args.sim_config)
+
+deg_param_names = None
+prot_param_names = None
+
+if n_bound + n_int == 0:
+    logger.error("No sample parameter requested")
+    sys.exit()
+
+if n_int > 0:
+    deg_int_samples, prot_int_samples = get_samples(
+        n_int=n_int,
+        deg_param_names=deg_param_names,
+        prot_param_names=prot_param_names,
+        sim_params=sim_params,
+        li_cons=li_cons,
+        uniform=True,
+    )
+if n_bound > 0:
+    deg_bound_samples, prot_bound_samples = get_bounding_samples(
+        n_bound=n_bound,
+        deg_param_names=deg_param_names,
+        prot_param_names=prot_param_names,
+        sim_params=sim_params,
+        li_cons=li_cons,
+    )
+
+if n_bound > 0 and n_int > 0:
+    deg_samples = np.vstack((deg_int_samples, deg_bound_samples))
+    if prot_int_samples is not None and prot_bound_samples is not None:
+        prot_samples = np.vstack((prot_int_samples, prot_bound_samples))
+if n_bound == 0:
+    deg_samples = deg_int_samples
+    prot_samples = prot_int_samples
+if n_int == 0:
+    deg_samples = deg_bound_samples
+    prot_samples = prot_bound_samples
+
+write_exec(
+    deg_samples,
+    deg_param_names=deg_param_names,
+    prot_param_names=prot_param_names,
+    folder_save=args.folder_save,
+    sim_params=sim_params,
+    prot_samples=prot_samples,
+)
