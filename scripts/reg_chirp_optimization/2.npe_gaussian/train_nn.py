118101826cfe8612ccbf45d0a1f15cb729e1cd82
Malik <malik.hassanaly@gmail.com>
2026-09-23 11:06:37 -0600

    adjust inputs so scaling is done inside the model

--- scripts/reg_chirp_optimization/2.npe_gaussian/train_nn.py
+++ scripts/reg_chirp_optimization/2.npe_gaussian/train_nn.py
@@ -3,7 +3,6 @@
 import os
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-import pickle
 import shutil
 import sys
 
@@ -19,10 +18,11 @@ from batfit.model.param_utils.train_utils import (
     train_model as train_model_param,
 )
 from batfit.model.paramNN import ProbParamCNN
+from batfit.preprocess.sim_setup import make_params
 from batfit.utils.data_utils import assemble_all_data
 from batfit.utils.torch_utils import (
     get_num_parameters,
-    make_dataset_from_np,
+    make_npe_dataset_from_np,
 )
 
 
@@ -44,21 +44,23 @@ def make_data_loaders(inp):
     Y_data = tmp["Y_data"]
 
     BATCH_SIZE = min(inp.batch_size, int(Y_data.shape[0] * 0.8))
-    loaders = make_dataset_from_np(
-        batch_size=BATCH_SIZE,
+    loaders, scalers = make_npe_dataset_from_np(
+        make_params(inp.sim_config),
         np_data=X_data,
         np_data_label=Y_data,
+        batch_size=BATCH_SIZE,
         test_split=0.1,
         val_split=0.1,
-        scale=True,
-        scale_y=False,
         save_path=data_root_folder,
     )
-    return loaders
+    return loaders, scalers
 
 
-def define_model(inp):
-    """Instantiate a ProbParamCNN from recipe parameters."""
+def define_model(inp, scaler_X=None):
+    """Instantiate a ProbParamCNN from recipe parameters.
+
+    scaler_X=None leaves a placeholder that load_state_dict fills.
+    """
     input_shape = (2, inp.n_points)
     model = ProbParamCNN(
         input_shape=input_shape,
@@ -67,22 +69,19 @@ def define_model(inp):
         fc_mu_list=[inp.num_fc_gamma_mu_units] * inp.num_fc_gamma_mu_hidden,
         fc_gamma_list=[inp.num_fc_gamma_mu_units] * inp.num_fc_gamma_mu_hidden,
         loss_fn=independent_normal_loss_param,
+        sim_config=inp.sim_config,
         cyc_mode=inp.cyc_mode,
         n_param_pred=inp.n_param_pred,
-        constrain_output=True,
-        dependent_outputs=False,
-        sim_config=inp.sim_config,
+        scaler_X=scaler_X,
+        param_margin=getattr(inp, "param_margin", 0.05),
     )
     num_parameters = get_num_parameters(model)
     logger.info(f"No. Trainable Parameters: {num_parameters}")
 
-    with open(inp.scaler_path, "rb") as f:
-        scaler_X = pickle.load(f)
-
-    return model, scaler_X
+    return model
 
 
-def do_training(inp, model, train_data_loader, test_data_loader, scaler_X):
+def do_training(inp, model, train_data_loader, test_data_loader):
     """Fit the model, selecting the best checkpoint on the test split."""
     noise_levels, a_min, a_max = make_noise_levels(
         target_mode=inp.target_mode,
@@ -93,6 +92,8 @@ def do_training(inp, model, train_data_loader, test_data_loader, scaler_X):
             2.01 * 2,
         ],
         cyc_mode=inp.cyc_mode,
+        vmin=model.sim_params["vmin"],
+        vmax=model.sim_params["vmax"],
     )
 
     train_model_param(
@@ -101,7 +102,6 @@ def do_training(inp, model, train_data_loader, test_data_loader, scaler_X):
         test_data_loader=test_data_loader,
         learning_rate=inp.lr,
         num_epochs=inp.epochs,
-        scaler_X=scaler_X,
         noise_levels=noise_levels,
         a_min=a_min,
         a_max=a_max,
@@ -114,7 +114,7 @@ def do_training(inp, model, train_data_loader, test_data_loader, scaler_X):
 
 if __name__ == "__main__":
     inp = ri.basic_input(sys.argv[1])
-    loaders = make_data_loaders(inp)
-    model, scaler_X = define_model(inp)
-    do_training(inp, model, loaders["train"], loaders["test"], scaler_X)
+    loaders, scalers = make_data_loaders(inp)
+    model = define_model(inp, scaler_X=scalers["X"])
+    do_training(inp, model, loaders["train"], loaders["test"])
     shutil.copy(sys.argv[1], os.path.join(inp.models_dir, "recipe.yml"))
